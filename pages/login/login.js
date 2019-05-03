3065bf8aeb29936c911591e00077be1e7411834e
nhannt97 <nhannt97hy@gmail.com>
2019-05-03 20:39:11 +0700

    update

--- pages/login/login.js
+++ pages/login/login.js
@@ -9,9 +9,7 @@ function login(apiService, $location) {
             password: self.password
         }, (res)=> {
             if(res) {
-                apiService.token = res.token.split('.')[1]
                 apiService.user = res.user
-                window.localStorage.token = res.token.split('.')[1]
                 window.localStorage.user = JSON.stringify(res.user)
                 $location.path('/message')
             }
